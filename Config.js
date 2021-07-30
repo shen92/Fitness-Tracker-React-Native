2163a9e3dc499dfcf0183ba5a4b0e50bb87e3886
Yingjie Shen <shen92@wisc.edu>
2021-07-30 02:46:09 -0700

    connect to server

--- Config.js
+++ Config.js
@@ -1,4 +1,5 @@
 export const Config = {
-  BASE_URL: "http://192.168.1.185:8080",
+  // BASE_URL: "http://192.168.1.185:8080",
+  BASE_URL: "https://fitness-tracker-rest-api.uw.r.appspot.com",
   // BASE_URL: "https://mysqlcs639.cs.wisc.edu",
 };
